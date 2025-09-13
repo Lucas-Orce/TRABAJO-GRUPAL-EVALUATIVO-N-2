46dc71c71d7d2335fbd47b86edb5424ec4b811ec
Luksfry <54043917+Lucas-Orce@users.noreply.github.com>
2025-09-12 22:31:56 -0300

    Add files via upload

---
+++ src/principal/MenuTransporte.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author Lucas
+ */
+public class MenuTransporte {
+    //aqui va todo lo que se le hace al transporte, o sea,
+    //   ALTA       BAJA      MODIFICACIONES
+    //      formato
+    //1- Altas
+    /*2- Bajas
+    3- Modificaciones
+    0- Salir
+    Ingrese su opción:*/
+    
+}
